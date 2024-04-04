59b0760cf68c050e990c55446586f20522901cf3
arelhan <are255gu@gmail.com>
2024-04-04 17:17:41 +0300

    Add project files.

---
+++ SplitPdf/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Diagnostics;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        string sourcePdfPath = GetFilePath();
+        string outputPath = GetFolderPath();
+
+        // Hedef klasör yoksa oluştur
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        // PdfReader nesnesi ile PDF dosyasını aç
+        PdfReader reader = new PdfReader(sourcePdfPath);
+
+        // Her bir sayfayı ayrı bir PDF olarak kaydet
+        for (int i = 1; i <= reader.NumberOfPages; i++)
+        {
+            string newFilePath = Path.Combine(outputPath, "Sayfa" + i + ".pdf");
+            int fileCount = 1;
+
+            // Eğer dosya zaten varsa, yeni bir isim oluştur
+            while (File.Exists(newFilePath))
+            {
+                newFilePath = Path.Combine(outputPath, "Sayfa" + i + "_" + fileCount + ".pdf");
+                fileCount++;
+            }
+
+            Document document = new Document();
+            PdfCopy copy = new PdfCopy(document, new FileStream(newFilePath, FileMode.Create));
+            document.Open();
+            copy.AddPage(copy.GetImportedPage(reader, i));
+            document.Close();
+        }
+
+        Console.WriteLine("İşlem tamamlandı. Ayrılmış PDF'ler kaydedildi: " + outputPath);
+        reader.Close();
+
+        // Belirlenen klasörü aç
+        Process.Start("explorer.exe", outputPath);
+    }
+
+    static string GetFilePath()
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.InitialDirectory = "c:\\";
+        openFileDialog.Filter = "PDF files (*.pdf)|*.pdf|All files (*.*)|*.*";
+        openFileDialog.FilterIndex = 1;
+        openFileDialog.RestoreDirectory = true;
+
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            return openFileDialog.FileName;
+        }
+        return null;
+    }
+
+    static string GetFolderPath()
+    {
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+        {
+            return folderBrowserDialog.SelectedPath;
+        }
+        return null;
+    }
+}
---
+++ SplitPdf/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SplitPdf")]
+[assembly: AssemblyDescription("PDF Split")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ARELHAN")]
+[assembly: AssemblyProduct("SplitPdf")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14ff1ed6-514b-4c23-9625-98275c88a2bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("tr-TR")]
